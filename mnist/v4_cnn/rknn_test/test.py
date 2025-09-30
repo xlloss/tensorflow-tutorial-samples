07b0eac21e23dd662618ad942668be369d352db5
slash.huang <slash.linux.c@gmail.com>
2025-09-30 10:53:35 +0800

    [RKNN] add rknn test

---
+++ mnist/v4_cnn/rknn_test/test.py
@@ -0,0 +1,119 @@
+import os
+import urllib
+import traceback
+import time
+import sys
+import numpy as np
+import cv2
+from rknn.api import RKNN
+
+ONNX_MODEL = 'model.onnx'
+RKNN_MODEL = 'v4cnn_onnx_model.rknn'
+
+
+def show_outputs(outputs):
+    output = outputs[0][0]
+    index = sorted(range(len(output)), key=lambda k : output[k], reverse=True)
+    fp = open('./labels.txt', 'r')
+    labels = fp.readlines()
+    top5_str = 'resnet50v2\n-----TOP 5-----\n'
+    for i in range(5):
+        value = output[index[i]]
+        if value > 0:
+            topi = '[{:>3d}] score:{:.6f} class:"{}"\n'.format(index[i], value, labels[index[i]].strip().split(':')[-1])
+        else:
+            topi = '[ -1]: 0.0\n'
+        top5_str += topi
+    print(top5_str.strip())
+
+
+def readable_speed(speed):
+    speed_bytes = float(speed)
+    speed_kbytes = speed_bytes / 1024
+    if speed_kbytes > 1024:
+        speed_mbytes = speed_kbytes / 1024
+        if speed_mbytes > 1024:
+            speed_gbytes = speed_mbytes / 1024
+            return "{:.2f} GB/s".format(speed_gbytes)
+        else:
+            return "{:.2f} MB/s".format(speed_mbytes)
+    else:
+        return "{:.2f} KB/s".format(speed_kbytes)
+
+
+def show_progress(blocknum, blocksize, totalsize):
+    speed = (blocknum * blocksize) / (time.time() - start_time)
+    speed_str = " Speed: {}".format(readable_speed(speed))
+    recv_size = blocknum * blocksize
+
+    f = sys.stdout
+    progress = (recv_size / totalsize)
+    progress_str = "{:.2f}%".format(progress * 100)
+    n = round(progress * 50)
+    s = ('#' * n).ljust(50, '-')
+    f.write(progress_str.ljust(8, ' ') + '[' + s + ']' + speed_str)
+    f.flush()
+    f.write('\r\n')
+
+
+if __name__ == '__main__':
+
+    # Create RKNN object
+    rknn = RKNN(verbose=True)
+
+    start_time = time.time()
+
+    # pre-process config
+    print('--> config model')
+    rknn.config(mean_values=[1.00], std_values=[1.00], target_platform='rk3588')
+    print('done')
+
+    # Load model
+    print('--> Loading model')
+    ret = rknn.load_onnx(model=ONNX_MODEL)
+    if ret != 0:
+        print('Load model failed!')
+        exit(ret)
+    print('done')
+
+    # Build model
+    print('--> Building model')
+    ret = rknn.build(do_quantization=True, dataset='./dataset.txt')
+    if ret != 0:
+        print('Build model failed!')
+        exit(ret)
+    print('done')
+
+    # Export rknn model
+    print('--> Export rknn model')
+    ret = rknn.export_rknn(RKNN_MODEL)
+    if ret != 0:
+        print('Export rknn model failed!')
+        exit(ret)
+    print('done')
+
+    # Set inputs
+    img = cv2.imread('./5.png')
+#    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = np.expand_dims(img, 0)
+    print(img.shape);
+    # Init runtime environment
+    print('--> Init runtime environment')
+    ret = rknn.init_runtime()
+    if ret != 0:
+        print('Init runtime environment failed!')
+        exit(ret)
+    print('done')
+
+    # Inference
+    print('--> Running model')
+    outputs = rknn.inference(inputs=[img], data_format=['nchw'])
+    np.save('./onnx_v4_cnn.npy', outputs[0])
+    x = outputs[0]
+    output = np.exp(x)/np.sum(np.exp(x))
+    outputs = [output]
+    show_outputs(outputs)
+    print('done')
+
+    rknn.release()
